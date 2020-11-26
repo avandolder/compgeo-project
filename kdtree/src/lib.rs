30ec2eff02fef7056cfdc869cc0097d7f6e30836
Adam Vandolder <adam.vandolder@gmail.com>
2020-11-25 21:40:54 -0500

    Use const generics for arbitrary dimensions

--- kdtree/src/lib.rs
+++ kdtree/src/lib.rs
@@ -1,45 +1,36 @@
-#![feature(box_syntax, box_patterns)]
+#![feature(box_syntax, box_patterns, min_const_generics)]
+
+use std::fmt::Debug;
+
+pub trait Element: PartialOrd + Clone + Debug {}
+impl<T: PartialOrd + Clone + Debug> Element for T {}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum KDTree {
-    Leaf((i32, i32)),
-    XSplit(Box<KDTree>, Box<KDTree>, i32),
-    YSplit(Box<KDTree>, Box<KDTree>, i32),
+pub enum KDTree<T: Element, const N: usize> {
+    Leaf([T; N]),
+    Split(Box<KDTree<T, N>>, Box<KDTree<T, N>>, T),
 }
 
-impl KDTree {
-    pub fn new(pts: &mut [(i32, i32)]) -> Option<Self> {
+impl<T: Element, const N: usize> KDTree<T, N> {
+    pub fn new(pts: &mut [[T; N]]) -> Option<Self> {
         if pts.is_empty() {
             None
         } else {
-            Some(Self::x_split(pts))
+            Some(Self::split(pts, 0))
         }
     }
 
-    fn x_split(pts: &mut [(i32, i32)]) -> Self {
-        pts.sort_by_key(|(x, _)| *x);
+    fn split(pts: &mut [[T; N]], depth: usize) -> Self {
+        pts.sort_by(|u, v| u[depth].partial_cmp(&v[depth]).unwrap());
         
         let mid = pts.len() / 2;
+        let next_depth = (depth + 1) % N;
         match pts.split_at_mut(mid) {
             ([], [pt]) => KDTree::Leaf(pt.clone()),
-            (left, right) => KDTree::XSplit(
-                box Self::y_split(left),
-                box Self::y_split(right),
-                left.last().unwrap().0,
-            ),
-        }
-    }
-
-    fn y_split(pts: &mut [(i32, i32)]) -> Self {
-        pts.sort_by_key(|(_, y)| *y);
-        
-        let mid = pts.len() / 2;
-        match pts.split_at_mut(mid) {
-            ([], [pt]) => KDTree::Leaf(pt.clone()),
-            (left, right) => KDTree::YSplit(
-                box Self::x_split(left),
-                box Self::x_split(right),
-                left.last().unwrap().1,
+            (left, right) => KDTree::Split(
+                box Self::split(left, next_depth),
+                box Self::split(right, next_depth),
+                left.last().unwrap()[depth].clone(),
             ),
         }
     }
@@ -47,35 +38,36 @@ impl KDTree {
 
 #[cfg(test)]
 mod tests {
-    use super::KDTree::{self, Leaf, XSplit, YSplit};
+    use super::KDTree::{self, Leaf, Split};
 
     #[test]
     fn empty() {
-        assert!(KDTree::new(&mut []).is_none());
+        assert!(KDTree::<i32, 2>::new(&mut []).is_none());
     }
 
     #[test]
     fn single_point() {
-        assert!(match KDTree::new(&mut [(0, 0)]) {
-            Some(Leaf((0, 0))) => true,
+        assert!(match KDTree::new(&mut [[0i32, 0]]) {
+            Some(Leaf([0, 0])) => true,
             _ => false,
         })
     }
 
     #[test]
     fn x_split() {
-        assert!(match KDTree::new(&mut [(0, 0), (1, 0)]) {
-            Some(XSplit(box Leaf((0, 0)), box Leaf((1, 0)), 0)) => true,
+        assert!(match KDTree::new(&mut [[0i32, 0], [1, 0]]) {
+            Some(Split(box Leaf([0, 0]), box Leaf([1, 0]), 0)) => true,
             _ => false,
         })
     }
 
     #[test]
     fn x_then_y_split() {
-        assert!(match KDTree::new(&mut [(0, 0), (1, 0), (0, 1)]) {
-            Some(XSplit(box Leaf((0, 0)),
-                        box YSplit(box Leaf((1, 0)),
-                                   box Leaf((0, 1)), 0), 0)) => true,
+        let tree = dbg!(KDTree::new(&mut [[0i32, 0], [1, 0], [0, 1]]));
+        assert!(match tree {
+            Some(Split(box Leaf([0, 0]),
+                       box Split(box Leaf([1, 0]),
+                                 box Leaf([0, 1]), 0), 0)) => true,
             _ => false,
         })
     }
